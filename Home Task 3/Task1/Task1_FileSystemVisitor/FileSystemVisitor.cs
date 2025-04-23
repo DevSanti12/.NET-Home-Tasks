f0c7155a7b5cd50c667c31a5ddb3c85ac3d0103b
Santiago Leyva <99155189+DevSanti12@users.noreply.github.com>
2025-04-23 13:24:24 -0500

    Added change to FileSystemVisitor

--- Home Task 3/Task1/Task1_FileSystemVisitor/FileSystemVisitor.cs
+++ Home Task 3/Task1/Task1_FileSystemVisitor/FileSystemVisitor.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Abstractions;
 
 namespace Task1_FileSystemVisitorApp;
 
 public partial class FileSystemVisitor
 {
     private readonly string _startFolder;
-    private readonly Func<string, bool> _fileFilter;       // Filter for files
-    private readonly Func<string, bool> _directoryFilter; // Filter for directories
+    private readonly Func<string, bool>? _fileFilter;       // Filter for files
+    private readonly Func<string, bool>? _directoryFilter; // Filter for directories
+    private readonly IFileSystem _fileSystem;
 
     //events for the 4 types of notifications
     public event EventHandler Started;
@@ -21,12 +23,14 @@ public partial class FileSystemVisitor
     private bool _abortSearch; // Flag to abort search
 
     // Constructor without a filter
-    public FileSystemVisitor(string startFolder)
+    public FileSystemVisitor(string startFolder, IFileSystem fileSystem)
     {
+        _fileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
+
         if (string.IsNullOrEmpty(startFolder))
             throw new ArgumentException("Starting folder cannot be null or empty", nameof(startFolder));
 
-        if (!Directory.Exists(startFolder))
+        if (!_fileSystem.Directory.Exists(startFolder))
             throw new DirectoryNotFoundException($"The directory '{startFolder}' does not exist.");
 
         _startFolder = startFolder;
@@ -34,8 +38,8 @@ public partial class FileSystemVisitor
         _directoryFilter = null; // No directory filter
     }
 
-    public FileSystemVisitor(string startFolder, Func<string, bool> filter)
-        : this(startFolder)
+    public FileSystemVisitor(string startFolder, IFileSystem fileSystem, Func<string, bool> filter)
+        : this(startFolder, fileSystem)
     {
         _fileFilter = filter;
         _directoryFilter = filter;
@@ -54,14 +58,15 @@ public partial class FileSystemVisitor
 
             if (_directoryFilter == null || _directoryFilter(currentFolder))
             {
-                yield return currentFolder; // Yield if there's no filter or directory doesn't pass the filter
+                // Yield if there's no filter or if there is a filter, yield only the matching folder
+                yield return currentFolder;
             }
 
             // Try to gather all subdirectories
             IEnumerable<string> subdirectories;
             try
             {
-                subdirectories = Directory.GetDirectories(currentFolder);
+                subdirectories = _fileSystem.Directory.GetDirectories(currentFolder);
             }
             catch (UnauthorizedAccessException)
             {
@@ -78,7 +83,7 @@ public partial class FileSystemVisitor
             IEnumerable<string> files;
             try
             {
-                files = Directory.GetFiles(currentFolder);
+                files = _fileSystem.Directory.GetFiles(currentFolder);
             }
             catch (UnauthorizedAccessException)
             {
@@ -90,6 +95,7 @@ public partial class FileSystemVisitor
             {
                 if ((_fileFilter == null || _fileFilter(file)))
                 {
+                    // Yield if there's no filter or if there is a filter, yield only the matching file 
                     yield return file;
                 }
             }        
